d3654ea6c08154f1927f8bc03053a6ce3306692f
kim.junhong <kim.junhong@navercorp.com>
2017-08-23 14:06:55 +0900

    모듈 설치 및 header 기본 추가

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+    render() {
+
+    	const loginButton = (
+            <li>
+                <a>
+                    <i className="material-icons">vpn_key</i>
+                </a>
+            </li>
+        );
+
+        const logoutButton = (
+            <li>
+                <a>
+                    <i className="material-icons">lock_open</i>
+                </a>
+            </li>
+        );
+        
+        return (
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <a className="brand-logo center">MEMOPAD</a>
+
+                    <ul>
+                        <li><a><i className="material-icons">search</i></a></li>
+                    </ul>
+
+                    <div className="right">
+                        <ul>
+                            { this.props.isLoggedIn ? logoutButton : loginButton }
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+Header.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    onLogout: PropTypes.func
+};
+
+Header.defaultProps = {
+    isLoggedIn: false,
+    onLogout: () => { console.error("logout function not defined");}
+};
+
+export default Header;

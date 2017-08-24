ff5e3e67879b8da18c8d02876f972eebd7c69ab4
kim.junhong <kim.junhong@navercorp.com>
2017-08-24 13:17:43 +0900

    header app으로 이동

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,14 +3,9 @@ import { Header } from '../components';
 
 class App extends React.Component {
     render() {
-        /* Check whether current route is login or register using regex */
-        let re = /(login|register)/;
-        let isAuth = re.test(this.props.location.pathname);
-
         return (
             <div>
-                {isAuth ? undefined : <Header/>}
-                { this.props.children }
+                <Header/>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import { Home, Login, Register } from './containers';
-import { Header } from './components';
+import { App } from './containers';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
 	<Router>
 		<div>
-			<Header />
+			<App />
     		<Route exact path="/" component={Home}/>
     		<Route path="/home" component={Home}/>
     		<Route path="/login" component={Login}/>

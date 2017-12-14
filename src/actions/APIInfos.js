7961c8aef2ed890f7d4e0fbc21b1357a25283779
kim.junhong <kim.junhong@navercorp.com>
2017-12-14 13:21:35 +0900

    api host 수정

--- src/actions/APIInfos.js
+++ src/actions/APIInfos.js
@@ -1,3 +1,4 @@
-export const MEMO_URL = 'post/api/memo';
-export const LOGIN_URL = 'post/api/auth/login';
-export const REGISTER_URL = 'post/api/auth/register';
+export const API_HOST = 'https://post.xargus.center/';
+export const MEMO_URL = API_HOST+'post/api/memo';
+export const LOGIN_URL = API_HOST+'post/api/auth/login';
+export const REGISTER_URL = API_HOST+'post/api/auth/register';

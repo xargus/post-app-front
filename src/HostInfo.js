976be2e00997f65185ddfcdcb359ca4c5e3be057
kim.junhong <kim.junhong@navercorp.com>
2018-02-02 10:32:54 +0900

    host 잘못된 부분 수정

--- src/HostInfo.js
+++ src/HostInfo.js
@@ -4,5 +4,5 @@ const HOST_REAL = "https://post.xargus.center/";
 const API_HOST_DEV = "http://localhost:8080/";
 const HOST_DEV = "http://localhost:3000/";
 
-export const API_HOST = API_HOST_DEV;
-export const HOST = HOST_DEV;
+export const API_HOST = API_HOST_REAL;
+export const HOST = HOST_REAL;
